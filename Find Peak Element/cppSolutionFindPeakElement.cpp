1f250e79a9946a907b12a3bdc57602fb54e3440b
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-04-04 13:30:30 +0530

    Create cppSolutionFindPeakElement.cpp

---
+++ Find Peak Element/cppSolutionFindPeakElement.cpp
@@ -0,0 +1,19 @@
+//problem link https://leetcode.com/problems/find-peak-element/
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        //The optimal approach will be with Binary Search
+        int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;        
+        while(s<e){
+            mid=s+(e-s)/2; 
+            
+            if(nums[mid]<nums[mid+1]){
+                s=mid+1;
+            }
+            else{e=mid;}
+        }
+        return s;
+    }
+};
